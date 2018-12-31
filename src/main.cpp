8ce1ed81fd6b194bf6dedb9e8ad796860da9b2d7
Jonathan Lovegren <44755365+easyvictor@users.noreply.github.com>
2018-12-30 22:25:33 -0500

    Update commets in header

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,19 @@
 /*
- NMEA0183/NMEA2000 library. NMEA2000 -> NMEA0183
-   Reads some messages from NMEA2000 and converts them to NMEA0183
-   format to NMEA0183_out (Serial on Arduino or /dev/tnt0 on RPi).
-   Also forwards all NMEA2000 bus messages in Actisense format.
+n2kconvert
+Converts NMEA2000 data to NMEA0183 sentences
+ 
+Reads some messages from NMEA2000 and converts them to NMEA0183
+sentence format, which is then output either to stdout or a named pipe.
 
-   The example is designed for sending N2k data to OpenCPN on RPi with PiCAN2.
-   It can be used also on PC with some Arduino/Teensy board to provide
-   NMEA0183 (and also NMEA2000) data to PC. Example has been tested on
-   RPi3B, ESP32, Arduino DUE, Arduino Mega and Teensy.
+This code is being used on a Raspberry Pi 3, with a PiCAN2 for CAN.
+It is generally combined with the program kplex to mux with 
+other NMEA0183 streams.
 
- To use this example you need install also:
-   - NMEA2000 library
-   - NMEA0183 library
-   - Related CAN libraries.
+To use this example you need install also:
 
- The example works with default settings on Arduino/Teensy boards and on
- Rasberry Pi 3 with PiCAN2 shield.
-
- On Rasberry Pi you need also Code Block environment and tty0tty (virtual
- null modem cable)
+- NMEA2000 library
+- NMEA_sfocketCAN library
+- NMEA0183 library
 */
 
 #include <NMEA2000_SocketCAN.h>
@@ -33,7 +28,7 @@
 // Reading serial number depends of used board. BoardSerialNumber module
 // has methods for RPi, Arduino DUE and Teensy. For others function returns
 // 0 and then DefaultSerialNumber will be used.
-#define DefaultSerialNumber 999999
+const uint32_t DefaultSerialNumber = 999999;
 
 // Set the information for other bus devices, which messages we support
 const unsigned long TransmitMessages[] = {0};
