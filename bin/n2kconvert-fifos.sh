b08ec89943f576dad17786909528b6e8c56e8fe2
Jonathan Lovegren <44755365+easyvictor@users.noreply.github.com>
2019-01-21 02:38:25 -0800

    Add support for default actisense serial forwarding

--- bin/n2kconvert-fifos.sh
+++ bin/n2kconvert-fifos.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-FIFO_NAME="/dev/n2kconvert"
+OUTPUT_FIFO="/dev/n2kconvert"
+FORWARD_FIFO="/dev/n2kforward"
 
 doquit() {
-   echo "Removing FIFO $FIFO_NAME and quitting."
-   rm "$FIFO_NAME"
+   echo "Removing FIFOs $OUTPUT_FIFO, $FORWARD_FIFO; then quitting."
+   rm "$OUTPUT_FIFO"
+   rm "$FORWARD_FIFO"
    exit 0
 }
 
@@ -11,8 +13,10 @@ trap 'doquit' QUIT
 trap 'doquit' INT
 trap 'doquit' TERM
 
-echo "Creating fifo: $FIFO_NAME"
-mkfifo "$FIFO_NAME"
+echo "Creating fifo: $OUTPUT_FIFO"
+mkfifo "$OUTPUT_FIFO"
+echo "Creating fifo: $FORWARD_FIFO"
+mkfifo "$FORWARD_FIFO"
 systemd-notify --ready
 while true ; do
   sleep 30
