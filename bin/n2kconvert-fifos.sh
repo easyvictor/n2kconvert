842ae9b7ff6fc267c9d4aa9621e83b0822386d87
Jonathan Lovegren <44755365+easyvictor@users.noreply.github.com>
2020-03-23 04:50:34 -0500

    Add config files for gpstimesync service
    Added supporting config changes for a separate python program which consumes the GPZDA (time and date) message and sets syste time based on it.

--- bin/n2kconvert-fifos.sh
+++ bin/n2kconvert-fifos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 OUTPUT_FIFO="/dev/n2kconvert"
 FORWARD_FIFO="/dev/n2kforward"
-
+TIME_FIFO="/dev/n2ktime"
 doquit() {
-   echo "Removing FIFOs $OUTPUT_FIFO, $FORWARD_FIFO; then quitting."
+   echo "Removing FIFOs $OUTPUT_FIFO, $FORWARD_FIFO, $TIME_FIFO; then quitting."
    rm "$OUTPUT_FIFO"
    rm "$FORWARD_FIFO"
+   rm "$TIME_FIFO"
    exit 0
 }
 
@@ -17,6 +18,8 @@ echo "Creating fifo: $OUTPUT_FIFO"
 mkfifo "$OUTPUT_FIFO"
 echo "Creating fifo: $FORWARD_FIFO"
 mkfifo "$FORWARD_FIFO"
+echo "Creating fifo: $TIME_FIFO"
+mkfifo "$TIME_FIFO"
 systemd-notify --ready
 while true ; do
   sleep 30
